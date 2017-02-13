78a4e2ddd1b47e44c2384a388c5cb7bdc50a6a22
Megan Talbot <meganftalbot@gmail.com>
2017-02-13 15:41:36 -0700

    Mark as read now working

--- app/assets/javascripts/mark_read.js.es6
+++ app/assets/javascripts/mark_read.js.es6
@@ -4,10 +4,9 @@ $( document ).ready(function(){
 
 function markAsRead(e) {
   e.preventDefault();
-
   var $link = $(this).parents('.link');
   var linkId = $link.data('link-id');
-
+  
   $.ajax({
     type: "PATCH",
     url: "/api/v1/links/" + linkId,
--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -2,14 +2,21 @@ require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
   scenario "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
+    user = User.create(name: "Megan", email: "megan@megan.com", password: "password")
+    link = user.links.create(url:"https://turing.io", title:"Turing")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
     visit "/"
+
     within('.link .read-status') do
       expect(page).to have_text("false")
     end
 
-    click_on "Mark as Read"
+    within('.link .mark-as-read') do
+      click_button "Mark as Read"
+    end
 
+    # expect(link.read).to eq(true)
     within('.link .read-status') do
       expect(page).to have_text("true")
     end
