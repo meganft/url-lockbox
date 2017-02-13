04e836cb9d19f0712edfb66400208c5f122c87d5
Megan Talbot <meganftalbot@gmail.com>
2017-02-13 11:38:56 -0700

    Initial url localbox commit from temp-mid-mod

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::LinksController < ApplicationController
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update_attributes(link_params)
+      render json: @link
+    else
+      render json: @link.errors.full_messages, status: 500
+    end
+  end
+
+  private
+
+  def link_params
+    params.permit(:read)
+  end
+end
